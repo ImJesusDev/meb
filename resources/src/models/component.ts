62a1ba5fbeab62a494a22099b4d980d902681f0c
Jesus Diaz <jesus740@gmail.com>
2021-07-02 16:39:46 -0500

    feat: add resources service

---
+++ resources/src/models/component.ts
@@ -0,0 +1,108 @@
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import mongoose from 'mongoose';
+
+/*
+ *   Interface that describes the properties
+ *   that are required to create a new Component
+ */
+export interface ComponentAttrs {
+  name: string;
+  resourceType: string;
+  componentBrand: string;
+  componentModel: string;
+  regularCondition: {
+    disables: boolean;
+    ticket: boolean;
+  };
+  badCondition: {
+    disables: boolean;
+    ticket: boolean;
+  };
+}
+
+/*
+ *   Interface that describes the properties
+ *   that a Component Document has
+ */
+interface ComponentDoc extends mongoose.Document {
+  name: string;
+  resourceType: string;
+  componentBrand: string;
+  componentModel: string;
+  regularCondition: {
+    disables: boolean;
+    ticket: boolean;
+  };
+  badCondition: {
+    disables: boolean;
+    ticket: boolean;
+  };
+}
+
+/*
+ *   Interface that describes the properties
+ *   that a Component Model has
+ */
+interface ComponentModel extends mongoose.Model<ComponentDoc> {
+  build(attrs: ComponentAttrs): ComponentDoc;
+}
+
+const componentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    resourceType: {
+      type: String,
+      required: true,
+    },
+    componentModel: {
+      type: String,
+      required: true,
+    },
+    componentBrand: {
+      type: String,
+      required: true,
+    },
+    regularCondition: {
+      disables: {
+        type: Boolean,
+      },
+      ticket: {
+        type: Boolean,
+      },
+    },
+    badCondition: {
+      disables: {
+        type: Boolean,
+      },
+      ticket: {
+        type: Boolean,
+      },
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+componentSchema.set('versionKey', 'version');
+componentSchema.plugin(updateIfCurrentPlugin);
+componentSchema.statics.build = (attrs: ComponentAttrs) => {
+  return new Component(attrs);
+};
+
+const Component = mongoose.model<ComponentDoc, ComponentModel>(
+  'Component',
+  componentSchema
+);
+
+export { Component };
---
+++ resources/src/models/document-type.ts
@@ -0,0 +1,76 @@
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+import mongoose from 'mongoose';
+
+/*
+ *   Interface that describes the properties
+ *   that are required to create a new DocumentType
+ */
+export interface DocumentTypeAttrs {
+  name: string;
+  resourceType: string;
+  disables: boolean;
+  requiresPhoto: boolean;
+}
+
+/*
+ *   Interface that describes the properties
+ *   that a DocumentType Document has
+ */
+interface DocumentTypeDoc extends mongoose.Document {
+  name: string;
+  resourceType: string;
+  disables: boolean;
+  requiresPhoto: boolean;
+}
+
+/*
+ *   Interface that describes the properties
+ *   that a DocumentType Model has
+ */
+interface DocumentTypeModel extends mongoose.Model<DocumentTypeDoc> {
+  build(attrs: DocumentTypeAttrs): DocumentTypeDoc;
+}
+
+const documentTypeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    resourceType: {
+      type: String,
+      required: true,
+    },
+    disables: {
+      type: Boolean,
+      required: true,
+    },
+    requiresPhoto: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+documentTypeSchema.set('versionKey', 'version');
+documentTypeSchema.plugin(updateIfCurrentPlugin);
+documentTypeSchema.statics.build = (attrs: DocumentTypeAttrs) => {
+  return new DocumentType(attrs);
+};
+
+const DocumentType = mongoose.model<DocumentTypeDoc, DocumentTypeModel>(
+  'DocumentType',
+  documentTypeSchema
+);
+
+export { DocumentType };
