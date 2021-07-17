8df09adeaaae0dfa8c77b70f95d3195914a9911b
Jesus Diaz <jesus740@gmail.com>
2021-07-17 10:45:20 -0500

    feat: add resources types index

---
+++ resources/src/routes/index-resource-types.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import { ResourceType } from '../models/resource-type';
+
+const router = express.Router();
+
+router.get(
+  '/api/resources/resource-types',
+  async (req: Request, res: Response) => {
+    const resources = await ResourceType.find({});
+
+    res.status(200).send(resources);
+  }
+);
+
+export default router;
