6174d16ca8892820c7cb2bc240fac887537022b9
Jesus Diaz <jesus740@gmail.com>
2021-08-16 10:29:33 -0500

    feat: add pagination and qrCode to resources

--- resources/src/routes/index-resource.ts
+++ resources/src/routes/index-resource.ts
@@ -2,11 +2,27 @@ import express, { Request, Response } from 'express';
 import { Resource } from '../models/resource';
 
 const router = express.Router();
+interface QueryParams {
+  page: number;
+  perPage: number;
+}
 
-router.get('/api/resources', async (req: Request, res: Response) => {
-  const resources = await Resource.find({}).populate(['documents']);
+interface Params {
+  id: string;
+}
+router.get(
+  '/api/resources',
+  async (req: Request<Params, {}, {}, QueryParams>, res: Response) => {
+    let perPage = req.query.perPage ? req.query.perPage : 50;
+    let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
 
-  res.status(200).send(resources);
-});
+    const resources = await Resource.find({})
+      .skip(Number(skip))
+      .limit(Number(perPage))
+      .populate(['documents']);
+
+    res.status(200).send(resources);
+  }
+);
 
 export default router;
