35f390a2ba8b98b91cd474d50cc43b5e9870e050
Jesus Diaz <jesus740@gmail.com>
2021-08-19 15:32:25 -0500

    feat: add filter to history

--- resources/src/routes/maintenance-history.ts
+++ resources/src/routes/maintenance-history.ts
@@ -6,10 +6,15 @@ const router = express.Router();
 router.get(
   '/api/resources/maintenances-history',
   async (req: Request<{}, {}, {}, any>, res: Response) => {
+    let query: any = {};
+    const status = req.query.status;
+    if (status) {
+      query['status'] = status;
+    }
     let perPage = req.query.perPage ? req.query.perPage : 50;
     let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
-    const totalResults = await Maintenance.find({}).countDocuments();
-    const maintenances = await Maintenance.find({})
+    const totalResults = await Maintenance.find(query).countDocuments();
+    const maintenances = await Maintenance.find(query)
       .skip(Number(skip))
       .limit(Number(perPage))
       .populate(['resource']);
--- resources/src/routes/repair-history.ts
+++ resources/src/routes/repair-history.ts
@@ -6,10 +6,15 @@ const router = express.Router();
 router.get(
   '/api/resources/repairs-history',
   async (req: Request<{}, {}, {}, any>, res: Response) => {
+    let query: any = {};
+    const status = req.query.status;
+    if (status) {
+      query['status'] = status;
+    }
     let perPage = req.query.perPage ? req.query.perPage : 50;
     let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
-    const totalResults = await Repair.find({}).countDocuments();
-    const repairs = await Repair.find({})
+    const totalResults = await Repair.find(query).countDocuments();
+    const repairs = await Repair.find(query)
       .skip(Number(skip))
       .limit(Number(perPage))
       .populate(['resource']);
