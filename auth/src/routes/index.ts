b57d23392b695565563598d6381ed8877fd8bceb
Jesus Diaz <jesus740@gmail.com>
2021-09-20 20:40:58 -0500

    feat: query users by client

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -7,8 +7,12 @@ const router = express.Router();
 router.get('/api/users', async (req: Request, res: Response) => {
   let query: any = {};
   const role = req.query.role as UserRole;
+  const client = req.query.client;
   if (role) {
     query['role'] = role;
+  }
+  if (client) {
+    query['client'] = client;
   }
   const users = await User.find(query);
   res.send(users);
