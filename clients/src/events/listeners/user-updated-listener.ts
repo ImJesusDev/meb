7b2bfc51ad823d790f2a50c3eb7acfcab9b6c1c9
Jesus Diaz <jesus740@gmail.com>
2021-09-30 17:21:15 -0500

    user updated event

--- clients/src/events/listeners/user-updated-listener.ts
+++ clients/src/events/listeners/user-updated-listener.ts
@@ -1,15 +1,16 @@
-import { Message } from 'node-nats-streaming';
-import { Subjects, Listener, UserUpdatedEvent } from '@movers/common';
-import { queueGroupName } from './queue-group-name';
-import { User } from '../../models/user';
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, UserUpdatedEvent } from "@movers/common";
+import { queueGroupName } from "./queue-group-name";
+import { User } from "../../models/user";
 
 export class UserUpdatedListener extends Listener<UserUpdatedEvent> {
   subject: Subjects.UserUpdated = Subjects.UserUpdated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: UserUpdatedEvent['data'], msg: Message) {
+  async onMessage(data: UserUpdatedEvent["data"], msg: Message) {
     const user = await User.findByEvent(data);
-
+    console.log("user updated event");
+    console.log(JSON.stringify(data, null, 2));
     if (!user) {
       return console.log(`User not found id: ${data.id}`);
     }
@@ -36,8 +37,8 @@ export class UserUpdatedListener extends Listener<UserUpdatedEvent> {
     user.set({
       id,
       email,
-      firstName,
-      lastName,
+      firstName: firstName ? firstName : user.firstName,
+      lastName: lastName ? lastName : user.lastName,
       client,
       office,
       photo,
