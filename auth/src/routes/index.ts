945fabc3931da7e94be1bc9ebae979b8ae544e40
Jesus Diaz <jesus740@gmail.com>
2021-10-21 19:07:10 -0500

    debug msg

--- auth/src/routes/index.ts
+++ auth/src/routes/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 /* Models */
-import { User } from '../models/user';
-import { UserRole } from '@movers/common';
+import { User } from "../models/user";
+import { UserRole } from "@movers/common";
 const router = express.Router();
 
 router.get(
-  '/api/users',
+  "/api/users",
   async (req: Request<{}, {}, {}, any>, res: Response) => {
     let query: any = {};
     const role = req.query.role as UserRole;
@@ -15,24 +15,25 @@ router.get(
     const status = req.query.status;
     let perPage = req.query.perPage ? req.query.perPage : 50;
     let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
-
+    console.log("perPage", perPage);
+    console.log("skip", skip);
     if (role) {
-      query['role'] = role;
+      query["role"] = role;
     }
     if (client) {
-      query['client'] = client;
+      query["client"] = client;
     }
-    query['deletedAt'] = null;
+    query["deletedAt"] = null;
 
-    if (status === 'archived') {
-      query['deletedAt'] = { $ne: null };
+    if (status === "archived") {
+      query["deletedAt"] = { $ne: null };
     }
 
     if (office) {
-      query['office'] = office;
+      query["office"] = office;
     }
     if (documentNumber) {
-      query['documentNumber'] = documentNumber;
+      query["documentNumber"] = documentNumber;
     }
     const totalResults = await User.find(query).countDocuments();
     const users = await User.find(query)
