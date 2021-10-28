c5a4657e7d33bcc397ff32806fb5a6a048bb0726
Jesus Diaz <jesus740@gmail.com>
2021-10-28 17:36:18 -0500

    update filters for repairs and maintenances

--- resources/src/routes/maintenance-history.ts
+++ resources/src/routes/maintenance-history.ts
@@ -1,34 +1,56 @@
-import express, { Request, Response } from 'express';
-import { Maintenance } from '../models/maintenance';
-
+import express, { Request, Response } from "express";
+import { Maintenance } from "../models/maintenance";
+import { Resource } from "../models/resource";
 const router = express.Router();
 
 router.get(
-  '/api/resources/maintenances-history',
+  "/api/resources/maintenances-history",
   async (req: Request<{}, {}, {}, any>, res: Response) => {
     let query: any = {};
+    let orClause: any = [];
     const status = req.query.status;
     const from = req.query.from;
     const to = req.query.to;
+    const client = req.query.client;
+    const office = req.query.office;
+    const resourceRef = req.query.resourceRef;
+
+    if (client && !office) {
+      const resources = await Resource.find({ client: client });
+      for (const resource of resources) {
+        orClause.push({ resourceRef: resource.reference });
+      }
+      query["$or"] = orClause;
+    }
+    if (office && !client) {
+      const resources = await Resource.find({ office: office });
+      for (const resource of resources) {
+        orClause.push({ resourceRef: resource.reference });
+      }
+      query["$or"] = orClause;
+    }
+    if (resourceRef) {
+      query["resourceRef"] = resourceRef;
+    }
 
     if (to) {
-      query['createdAt'] = {
+      query["createdAt"] = {
         $lte: new Date(to),
       };
     }
     if (from) {
-      query['createdAt'] = {
+      query["createdAt"] = {
         $gte: new Date(from),
       };
     }
     if (from && to) {
-      query['createdAt'] = {
+      query["createdAt"] = {
         $gte: new Date(from),
         $lte: new Date(to),
       };
     }
     if (status) {
-      query['status'] = status;
+      query["status"] = status;
     }
     let perPage = req.query.perPage ? req.query.perPage : 50;
     let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
@@ -36,7 +58,7 @@ router.get(
     const maintenances = await Maintenance.find(query)
       .skip(Number(skip))
       .limit(Number(perPage))
-      .populate(['resource', 'assignedUser']);
+      .populate(["resource", "assignedUser"]);
 
     res.status(200).send({
       maintenances,
