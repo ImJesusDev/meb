7773e7eb5676e317f1b20eba173284a3d8799052
Jesus Diaz <jesus740@gmail.com>
2021-12-27 14:19:34 -0500

    update query

--- resources/src/routes/index-resource.ts
+++ resources/src/routes/index-resource.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { Resource } from '../models/resource';
-
+// import { Client } from '../models/client';
+// import { Office } from '../models/office';
+import { currentUser, ResourceStatus, UserRole } from '@movers/common';
 const router = express.Router();
 interface QueryParams {
   page: number;
@@ -19,8 +21,11 @@ interface Params {
 }
 router.get(
   '/api/resources',
-  async (req: Request<Params, {}, {}, QueryParams>, res: Response) => {
+  currentUser,
+  async (req: Request<any, any, any, any>, res: Response) => {
     let query: any = {};
+    let allClients: any;
+    let clientOffices: any;
     const status = req.query.status;
     const client = req.query.client;
     const office = req.query.office;
@@ -28,12 +33,14 @@ router.get(
     const reference = req.query.reference;
     const from = req.query.from;
     const to = req.query.to;
+    const user = req.currentUser;
+
     if (status) query['status'] = status;
     if (client) query['client'] = client;
     if (office) query['office'] = office;
     if (type) query['type'] = type;
     if (reference) query['reference'] = reference;
-
+    
     if (to) {
       query['createdAt'] = {
         $lte: new Date(to),
@@ -50,9 +57,60 @@ router.get(
         $lte: new Date(to),
       };
     }
+    let orClause = [];
+    if(status === ResourceStatus.PendingCheckup) {
+      orClause.push({ status: ResourceStatus.Checkup })
+    }
+    if(status === ResourceStatus.PendingMaintenance) {
+      orClause.push({ status: ResourceStatus.Maintenance })
+    }
+    if(status === ResourceStatus.PendingRepair) {
+      orClause.push({ status: ResourceStatus.Repair })
+    }
+    query['$or'] = orClause;
+    // Here we validate the role of the current user
+    // if (user) {
+    //   switch (user.role) {
+    //     case UserRole.MebSuperAdmin:
+    //       // Find all clients for the admin
+    //       allClients = await Client.find({ mebSuperAdmin: user.id });
+    //       for (const client of allClients) {
+    //         // Find all offices of the client
+    //         const clientOffices = await Office.find({ client: client.name });
+    //         // Create or clause to find resources of the admin offices
+    //         for (const office of clientOffices) {
+    //           orClause.push({ office: office.name });
+    //         }
+    //       }
+    //       query['$or'] = orClause;
+    //       break;
+    //     case UserRole.MebAdmin:
+    //       // Find all offices of the admin
+    //       clientOffices = await Office.find({ mebAdmin: user.id });
+    //       console.log('clientOffices', clientOffices);
+    //       // Create or clause to find resources of the admin offices
+    //       for (const office of clientOffices) {
+    //         orClause.push({ office: office.name });
+    //       }
+    //       query['$or'] = orClause;
+    //       break;
+    //     case UserRole.Client:
+    //       // Find all offices of the admin
+    //       clientOffices = await Office.find({ clientAdmin: user.id });
+    //       // Create or clause to find resources of the admin offices
+    //       for (const office of clientOffices) {
+    //         orClause.push({ office: office.name });
+    //       }
+    //       query['$or'] = orClause;
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // }
+
+    console.log("FINAL QUERY", JSON.stringify(query, null, 2))
     let perPage = req.query.perPage ? req.query.perPage : 50;
     let skip = req.query.page ? (Math.max(0, req.query.page) - 1) * perPage : 0;
-
     const totalResults = await Resource.find(query).countDocuments();
     const resources = await Resource.find(query)
       .skip(Number(skip))
